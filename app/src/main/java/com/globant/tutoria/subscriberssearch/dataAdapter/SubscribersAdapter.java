4145f59878f4034e818feb791268dd0583d689d3
Juan Pablo <juan.herrera@globant.com>
2016-08-23 22:59:21 -0500

    Corrections
    I adjusted the code, according the Julio coments's

--- app/src/main/java/com/globant/tutoria/subscriberssearch/dataAdapter/SubscribersAdapter.java
+++ app/src/main/java/com/globant/tutoria/subscriberssearch/dataAdapter/SubscribersAdapter.java
@@ -14,10 +14,10 @@ import java.util.List;
 /**
  * Created by juan.herrera@globant.com on 20/08/2016.
  */
-public class DataSubscribersAdapter extends RecyclerView.Adapter<DataSubscribersAdapter.ViewHolder> {
+public class SubscribersAdapter extends RecyclerView.Adapter<SubscribersAdapter.ViewHolder> {
 
     private List<SubscribersModel> listSubscribers;
-    public DataSubscribersAdapter (List<SubscribersModel> listSubscribers){
+    public SubscribersAdapter(List<SubscribersModel> listSubscribers){
         this.listSubscribers = listSubscribers;
     }
 
@@ -31,13 +31,13 @@ public class DataSubscribersAdapter extends RecyclerView.Adapter<DataSubscribers
     }
 
     @Override
-    public DataSubscribersAdapter.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+    public SubscribersAdapter.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
         View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.row_subscriber, viewGroup, false);
         return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(DataSubscribersAdapter.ViewHolder viewHolder, int position) {
+    public void onBindViewHolder(SubscribersAdapter.ViewHolder viewHolder, int position) {
         viewHolder.txtSingleSubscriber.setText(this.listSubscribers.get(position).getLogin());
     }
 
